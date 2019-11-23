69f572b60d0fda9f4b3f0d8e5ae86c155a9cb0a9
SergiiYurko <57231766+SergiiYurko@users.noreply.github.com>
2019-11-23 17:00:41 +0200

    add private fields

--- modules/NodeWrapper.py
+++ modules/NodeWrapper.py
@@ -4,10 +4,10 @@ import json
 
 class NodeWrapper:
     def __init__(self, name, ip, port):
-        self.name = name
-        self.ip = ip
-        self.port = port
-        self.stats = {}
+        self.__name = name
+        self.__ip = ip
+        self.__port = port
+        self.__stats = {}
 
     def sendRequest(self, request):
         requests.post("http://" + self.ip + ":" + self.port, data=request)
@@ -16,4 +16,12 @@ class NodeWrapper:
         self.stats = requests.post("http://" + self.ip + ":" + self.port)
         # TODO Чекаєм на реалізацію хендлу запиту стати
 
+    def getName(self):
+        return self.__name
     
+    def getIp(self):
+        return self.__ip
+    
+    def getPort(self):
+        return self.__port
+                
