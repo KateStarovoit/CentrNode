d30e25529ade0e4b467f103b39c14f8b449dec0f
Serhiy <pylypchuk.serhiy@gmail.com>
2019-11-27 15:50:08 +0200

    Remade the way we get the servers

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import modules.NodeWrapper as nw
-import modules.NodeAnalyzer as na
+import modules.LoadAnalyzer as la
 import flask
 import queue
+import json
+import socket
+
+
+def getPort():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 0))
+    s.listen(1)
+    the_port = s.getsockname()[1]
+    s.close()
+
+    return the_port
+
+
+def getIp():
+    return socket.gethostbyname(socket.gethostname())
+
 
 Server = flask.Flask(__name__)
-ip = "localhost"
-port = "8888"
+ip = getIp()
+port = getPort()
 
-node_number = 1  # temporary
+file = open("stats.json", "r")
+NodeDictList = json.load(file)
+NodeDictList.append({"name": socket.gethostname(), "ip": ip, "port": port, "stats": {}})
+file.close()
+
+file = open("stats.json", "w")
+json.dump(NodeDictList, file, indent=4)
+file.close()
+
+node_number = len(NodeDictList)  # temporary
 NodeList = []
 
 MessageQueue = queue.Queue()
 
 # TODO Написати нормальний механізм додавання нових нод
 for i in range(node_number):
-    name = "temp"
-    ip = "localhost"
-    port = "8888"
-    NodeList.append(nw.NodeWrapper(name, ip, port))
+    NodeList.append(nw.NodeWrapper(NodeDictList[i]["name"], NodeDictList[i]["ip"], NodeDictList[i]["port"]))
 
 
 @Server.route('/send_message/')
@@ -29,7 +52,7 @@ def init():
 # //////////////
 # TODO Цей код повинен періодично виконуватись
 if NodeList and MessageQueue:
-    node_index = na.NodeAnalyzer(NodeList)
+    node_index = la.LoadAnalyzer(NodeList)
     NodeList[node_index].sendMessage(MessageQueue.get())
 # //////////////
 
