8bace780c24b98cafbb9a79690b69bd569c47706
Gordiy <gordiy.yuriy.gord123@gmail.com>
2019-12-16 00:22:29 +0200

    created utils.py file for additional methods. And created function for reading stats

---
+++ utils.py
@@ -0,0 +1,10 @@
+import os
+import json
+
+def get_statistics(path_to_file):
+    if os.path.exists(path=path_to_file):
+        with open(path_to_file) as stat_file:
+            data = json.load(stat_file)
+    else:
+        data = {'Error!!!' : "File not found."}
+    return data
