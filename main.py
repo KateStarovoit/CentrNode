ed13fd6f3e8c97bd996533b56322a7fb20efd303
Slayrak <322TOPGAMEOFTHEWORLD228@gmail.com>
2019-12-17 12:33:24 +0200

    adding simultaneous launch of server and function

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import modules.NodeWrapper as nw
 import modules.LoadAnalyzer as la
 import flask
 import queue
-import threading
+import multiprocessing
 import time
+import sched
+import json
 
 Server = flask.Flask(__name__)
 ip = "localhost"
 port = "8888"
 
-node_number = 1  # temporary
 NodeList = []
 
-MessageQueue = queue.Queue()
+MessageQueue = queue.Queue(maxsize=100)
 
-# TODO Написати нормальний механізм додавання нових нод
-for i in range(node_number):
-    name = "temp"
-    ip = "localhost"
-    port = "8888"
+
+@Server.route('/add_node/')
+def init():
+    content = json.loads(flask.request.json)
+    name = content["name"]
+    ip = content["ip"]
+    port = content["port"]
     NodeList.append(nw.NodeWrapper(name, ip, port))
 
 
@@ -28,21 +31,34 @@ def init():
     MessageQueue.put(message)
 
 
-# //////////////
-# TODO Цей код повинен періодично виконуватись
-def sendMessage():
+@Server.route('/login/')
+def check_login():
+    login_base = json.load(open("login_database.json"))
+    content = json.loads(flask.request.json)
+    if content["login"] in login_base:
+        return True
+    else:
+        return False
+
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def pullStats():
     while True:
-        time.sleep(5)
-        if NodeList and MessageQueue:
+        if len(NodeList) > 0 and MessageQueue.qsize() > 0:
             node_index = la.LoadAnalyzer(NodeList)
             NodeList[node_index].sendMessage(MessageQueue.get())
 
-# ticker = threading.Event()
-# while not ticker.wait(5):
-#     sendMessage()
 
-# //////////////
-
-if __name__ == '__main__':
+def serverRun():
     Server.run(host=ip, port=port)
 
+
+if __name__ == '__main__':
+    server_process = multiprocessing.Process(target=serverRun)
+    pullStats_process = multiprocessing.Process(target=pullStats)
+    server_process.start()
+    pullStats_process.start()
+    server_process.join()
+    pullStats_process.join()
